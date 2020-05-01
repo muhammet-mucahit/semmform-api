ee19d81d1b67a0fe97ddba32c7c21daa50a88f98
sevkioruc <sevkioruc34@gmail.com>
2020-05-01 04:02:25 +0300

    formfield ekleme eklendi

--- form/urls.py
+++ form/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from form.views import FormList, FormDetail, FormfieldList
+from form.views import FormList, FormDetail, FormFieldDetail, FormFieldList
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('forms/', FormList.as_view(), name="forms"),
     path('forms/<int:pk>/', FormDetail.as_view(), name="form-detail"),
-    path('formfield/<int:pk>', FormfieldList.as_view(), name="formfield")
+    path('formfields/', FormFieldList.as_view(), name="formfields"),
+    path('formfields/<int:pk>', FormFieldDetail.as_view(), name="formfield")
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
